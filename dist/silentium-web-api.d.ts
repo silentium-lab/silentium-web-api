cccdec6fe9d6cce5b10ae749ff8d80d01f57bb9f
Eugene Kosukhin <kosuha606@gmail.com>
2025-10-11 17:54:55 +0300

    fix(main): build

--- dist/silentium-web-api.d.ts
+++ dist/silentium-web-api.d.ts
@@ -1,4 +1,4 @@
-import { DataType, DataUserType } from 'silentium';
+import { EventType, EventUserType, DataType } from 'silentium';
 
 interface FetchRequestType {
     baseUrl?: string;
@@ -14,23 +14,23 @@ interface FetchRequestType {
  * https://kosukhin.github.io/patron-web-api/#/fetch/fetched
  * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
  */
-declare const fetchedData: (requestSrc: DataType<Partial<FetchRequestType>>, errorOwner?: DataUserType, abortSrc?: DataType) => DataType<string>;
+declare const fetchedData: (requestSrc: EventType<Partial<FetchRequestType>>, errorOwner?: EventUserType, abortSrc?: EventType) => EventType<string>;
 
 /**
  * Represents a request for JSON data.
  */
-declare const requestJson: (requestSrc: DataType<Partial<FetchRequestType>>, errorOwner?: DataUserType<unknown>) => DataType<Partial<FetchRequestType>>;
+declare const requestJson: (requestSrc: EventType<Partial<FetchRequestType>>, errorOwner?: EventUserType<unknown>) => EventType<Partial<FetchRequestType>>;
 
 /**
  * Represents a collection of elements that match a given CSS selector.
  */
-declare const elements: <T extends HTMLElement>(selectorSrc: DataType<string>) => DataType<T[]>;
+declare const elements: <T extends HTMLElement>(selectorSrc: EventType<string>) => EventType<T[]>;
 
 /**
  * Helps to print logs to somewhere
  * https://developer.mozilla.org/en-US/docs/Web/API/Console_API
  */
-declare const log: <T>(sourceSrc: DataType<T>, titleSrc: DataType<string>) => DataType<T>;
+declare const log: <T>(sourceSrc: EventType<T>, titleSrc: EventType<string>) => EventType<T>;
 
 declare const timer: (delay: number) => DataType<number>;
 
